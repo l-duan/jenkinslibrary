5196b27b39377ed600efe54fbcd4d460d9893839
Christian Duan <christianbadguy@126.com>
2020-04-08 00:23:49 +0800

    Update etc.groovy

--- src/org/devops/etc.groovy
+++ src/org/devops/etc.groovy
@@ -2,7 +2,7 @@ package org.devops
 
 //封装HTTP请求
 def HttpReq(reqType,reqUrl,reqBody){
-    def etcdServer = "http://49.233.222.216:2376/v3"
+    def etcdServer = "http://49.233.222.216:2379/v3"
       result = httpRequest httpMode: reqType, 
                 contentType: "APPLICATION_JSON",
                 consoleLogResponseBody: true,
